1b67368bd16ff165d662696fb2e8ddaf2fa06dd2
Md. Imran Hossain <sandsoftimer@gmail.com>
2022-02-21 17:36:05 +0600

    Ibrahim vai... uv solution working.... fill gap is in progress...

---
+++ Assets/Scripts/MeshEraser.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshEraser : APBehaviour
+{
+    public MeshUVPainter meshUVPainter = new MeshUVPainter();
+    public GameObject foldingObj;
+    public SkinnedMeshRenderer foldinMeshFilter;
+    public Material outputMaterial;
+    public Color paintColor;
+    public float paintRadious;
+    public Vector2 rectengle;
+
+    Texture2D outputTex;
+    float bendValue = 0;
+    RaycastHit preHit;
+    #region ALL UNITY FUNCTIONS
+
+    // Awake is called before Start
+    public override void Awake()
+    {
+        base.Awake();
+        outputMaterial.SetTexture("MaskInput",
+                outputMaterial.GetTexture("Texture"));
+    }
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        //if (gameState.Equals(GameState.GAME_INITIALIZED) && Input.GetMouseButtonDown(0))
+        //{
+        //    gameManager.ChangeGameState(GameState.GAME_PLAY_STARTED);
+        //    gameState = GameState.GAME_PLAY_STARTED;
+        //}
+
+        //if (!gameState.Equals(GameState.GAME_PLAY_STARTED))
+        //    return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            preHit = new RaycastHit();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit raycastHit = new RaycastHit();
+            raycastHit.GetRaycastFromScreenTouch();
+            if (raycastHit.collider != null)
+            {
+                bendValue += Time.deltaTime * 20;
+                foldingObj.transform.position = raycastHit.point;
+                //foldingObj.transform.position = Vector3.Lerp(
+                //    transform.position,
+                //    raycastHit.point,
+                //    0.95f);
+                foldingObj.transform.rotation = Quaternion.Lerp(
+                    foldingObj.transform.rotation,
+                    Quaternion.LookRotation(raycastHit.point - previousPosition, Vector3.up),
+                    Time.deltaTime * 5);
+                //foldingObj.transform.localEulerAngles = new Vector3(0,(previousPosition - raycastHit.point).y - 90, 0);
+                //Vector3 look = new Vector3(0, (previousPosition - raycastHit.point).y - 90, 0);
+                //foldingObj.transform.localEulerAngles = previousPosition - raycastHit.point;
+                outputTex = meshUVPainter.PaintOnUV(raycastHit, preHit, paintColor, paintRadious, 1000, rectengle);
+                outputMaterial.SetTexture("MaskInput", outputTex);
+
+                bendValue = Mathf.Clamp(bendValue, 0, 100);
+                foldinMeshFilter.SetBlendShapeWeight(0, bendValue);
+                preHit = raycastHit;
+
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!gameState.Equals(GameState.GAME_PLAY_STARTED))
+            return;
+
+    }
+
+    Vector3 previousPosition;
+    void LateUpdate()
+    {
+        if (!gameState.Equals(GameState.GAME_PLAY_STARTED))
+            return;
+        previousPosition = foldingObj.transform.position;
+    }
+
+    #endregion ALL UNITY FUNCTIONS
+    //=================================   
+    #region ALL OVERRIDING FUNCTIONS
+    
+    
+    #endregion ALL OVERRIDING FUNCTIONS
+    //=================================
+    #region ALL SELF DECLEAR FUNCTIONS
+    
+    
+    #endregion ALL SELF DECLEAR FUNCTIONS
+
+}
---
+++ Assets/Scripts/MeshUVPainter.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+
+//[System.Serializable]
+public class MeshUVPainter
+{   
+    public MeshRenderer meshRenderer;
+    public MeshFilter filter;
+    public Collider lastCollider;
+
+    public Material material;
+    public Texture2D texture;
+    public Mesh mesh;
+
+    Vector3[] vertices;
+    Color[] pixelBuffer;
+    Color oldColor;
+
+    public int vCount;
+    public float progress;
+    float d;
+    float f;
+    float delta;
+    float[] progVerts;
+
+    public Texture2D PaintOnUV(RaycastHit raycast,RaycastHit prevhit, Color color, float radius, float sharpness,  Vector2 rectangle)
+    {
+        if (lastCollider != raycast.collider)
+        {
+            InitializeNewMesh(raycast.collider);
+        }
+
+        delta = Time.deltaTime;
+        Vector2 uv = raycast.textureCoord * ((texture.width) - 1);
+        Vector2 preuv= prevhit.textureCoord * ((texture.width) - 1);
+        if (uv.x == preuv.x && uv.y == preuv.y)
+            return texture;
+        Vector2 dir = (uv - preuv).normalized;
+        for (int u = 0; u < (texture.width); u++)
+        {
+            for (int v = 0; v < (texture.width); v++)
+            {
+                float distSqr = (u - uv.x) * (u - uv.x) + (v - uv.y) * (v - uv.y);
+                Vector2 newv = new Vector2(((u - uv.x) * dir.y) + (v - uv.y) * (-dir.x), ((u - uv.x) * dir.x) + (v - uv.y) * (dir.y));
+
+                //if ((u < uv.x + (rectangle.x / 2)) && (u > uv.x - (rectangle.x / 2)) && (v < uv.y + (rectangle.y / 2)) && (v > uv.y - (rectangle.y / 2)))
+                if ((newv.x < rectangle.x / 2) && (newv.x > -rectangle.x / 2) && (newv.y < rectangle.y / 2) && (newv.y > -rectangle.y / 2))
+                {
+                    d = Mathf.Sqrt(distSqr);
+                    f = sharpness * delta * ((radius - d) / radius);
+                    oldColor = pixelBuffer[v * (texture.width) + u];
+                    if (oldColor.a != 0)
+                    {
+                        oldColor.r = Mathf.Lerp(oldColor.r, color.r, f);
+                        oldColor.g = Mathf.Lerp(oldColor.g, color.g, f);
+                        oldColor.b = Mathf.Lerp(oldColor.b, color.b, f);
+                        oldColor.a = Mathf.Clamp01(oldColor.a + f);
+                        //Debug.Log(cOld.a);
+                    }
+                    else
+                    {
+                        oldColor = color;
+                        oldColor.a = f;
+                    }
+                    oldColor.a = 0;
+
+                    pixelBuffer[v * (texture.width) + u] = oldColor;
+                }
+            }
+        }
+        texture.SetPixels(pixelBuffer);
+        texture.Apply();
+
+        //Vector3 hitpoint = filter.transform.InverseTransformPoint(raycast.point);
+        //progress = 0;
+        //for (int i = 0; i < vCount; i++)
+        //{
+        //    d = Vector3.SqrMagnitude(hitpoint - vertices[i]);
+        //    if (d < vRad * vRad)
+        //    {
+        //        //d = Mathf.Sqrt(d);
+        //        progVerts[i] = Mathf.Clamp01(progVerts[i] + 1);
+        //    }
+        //    progress += progVerts[i];
+        //}
+
+        return texture;
+    }
+
+    void InitializeNewMesh(Collider collider)
+    {
+        meshRenderer = collider.GetComponent<MeshRenderer>();
+        filter = collider.GetComponent<MeshFilter>();
+
+        material = meshRenderer.material;
+        mesh = filter.mesh;
+        vCount = filter.mesh.vertexCount;
+        vertices = filter.mesh.vertices;
+        progVerts = new float[vCount];
+        progress = 0;
+
+        texture = MonoBehaviour.Instantiate(material.mainTexture as Texture2D);
+        pixelBuffer = texture.GetPixels();
+        material.SetTexture("_BaseMap", texture);
+    }
+}
