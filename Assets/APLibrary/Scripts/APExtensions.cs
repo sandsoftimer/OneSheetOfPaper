2939c8c3331ce6c5b03ea0cfcbdafd6e302b1aad
Md. Imran Hossain <sandsoftimer@gmail.com>
2022-02-23 16:01:28 +0600

    Working as intended....

--- Assets/APLibrary/Scripts/APExtensions.cs
+++ Assets/APLibrary/Scripts/APExtensions.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public static class APExtensions
 {
+    public static void TryUpdateShapeToAttachedSprite(this PolygonCollider2D collider)
+    {
+        collider.UpdateShapeToSprite(collider.GetComponent<SpriteRenderer>().sprite);
+    }
+
+    public static void UpdateShapeToSprite(this PolygonCollider2D collider, Sprite sprite)
+    {
+        // ensure both valid
+        if (collider != null && sprite != null)
+        {
+            // update count
+            collider.pathCount = sprite.GetPhysicsShapeCount();
+
+            // new paths variable
+            List<Vector2> path = new List<Vector2>();
+
+            // loop path count
+            for (int i = 0; i < collider.pathCount; i++)
+            {
+                // clear
+                path.Clear();
+                // get shape
+                sprite.GetPhysicsShape(i, path);
+                // set path
+                collider.SetPath(i, path.ToArray());
+            }
+        }
+    }
+
     public static Texture2D Texture2D(this RenderTexture renderTexture)
     {
         Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
--- Assets/Scripts/MeshEraser.cs
+++ Assets/Scripts/MeshEraser.cs
@@ -11,12 +11,15 @@ public class MeshEraser : APBehaviour
     public float paintRadious, draggingThreshold = 0.25f, foldingSpeed = 50;
     public float movingSpeed = 0.38f, foldingScalingSpeed = 30f;
     public Vector2 rectengle;
+    public SpriteRenderer spriteRenderer;
+    public PolygonCollider2D polygonCollider2D;
+    public Transform inputChecker;
 
     GameObject foldingObj;
     SkinnedMeshRenderer foldinMesh;
     Texture2D outputTex;
     float bendValue = 0;
-    bool firstTry;
+    bool dragging, firstTry;
     RaycastHit preHit;
     #region ALL UNITY FUNCTIONS
 
@@ -49,54 +52,64 @@ public class MeshEraser : APBehaviour
         {
             preHit = new RaycastHit();
             preHit.GetRaycastFromScreenTouch();
+            dragging = true;
             firstTry = true;
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && dragging)
         {
             RaycastHit currentRayCastHit = new RaycastHit();
             currentRayCastHit.GetRaycastFromScreenTouch();
-            if (currentRayCastHit.collider != null)
+            if (DraggingOnPaper(currentRayCastHit.point))
             {
-                if (firstTry)
+                if (currentRayCastHit.collider != null)
                 {
-                    meshUVPainter = new MeshUVPainter();
-                    meshUVPainter.fl = 0;
-                    meshUVPainter.count = 0;
-                    bendValue = 0;
-                    foldingScalingSpeed = 0;
-                    foldingObj = Instantiate(foldingPrefab, transform);
-                    foldingObj.transform.position = currentRayCastHit.point;
-                    foldinMesh = foldingObj.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
-                    foldingObj.transform.localScale = new Vector3(1, 0, 0);
-                    firstTry = false;
-                }
+                    if (firstTry)
+                    {
+                        meshUVPainter = new MeshUVPainter();
+                        meshUVPainter.fl = 0;
+                        meshUVPainter.count = 0;
+                        bendValue = 0;
+                        foldingScalingSpeed = 0;
+                        foldingObj = Instantiate(foldingPrefab, transform);
+                        foldingObj.transform.position = currentRayCastHit.point;
+                        foldinMesh = foldingObj.transform.GetChild(0).GetComponent<SkinnedMeshRenderer>();
+                        foldingObj.transform.localScale = new Vector3(1, 0, 0);
+                        firstTry = false;
+                    }
 
-                foldingObj.transform.position = Vector3.Lerp(foldingObj.transform.position, currentRayCastHit.point, movingSpeed * Time.deltaTime);
-                //foldingObj.transform.LookAt(currentRayCastHit.point);
-                foldingObj.transform.rotation = Quaternion.Lerp(
-                        foldingObj.transform.rotation,
-                        Quaternion.LookRotation(currentRayCastHit.point - preHit.point, Vector3.up),
-                        0.75f);
+                    foldingObj.transform.position = Vector3.Lerp(foldingObj.transform.position, currentRayCastHit.point, movingSpeed * Time.deltaTime);
+                    //foldingObj.transform.LookAt(currentRayCastHit.point);
+                    foldingObj.transform.rotation = Quaternion.Lerp(
+                            foldingObj.transform.rotation,
+                            Quaternion.LookRotation(currentRayCastHit.point - preHit.point, Vector3.up),
+                            0.75f);
 
-                if ((currentRayCastHit.point - preHit.point).magnitude > draggingThreshold)
-                {
-                    bendValue += Time.deltaTime * foldingScalingSpeed;
-                    Physics.Raycast(new Ray(foldingObj.transform.position.ModifyThisVector(0, 1, 0), Vector3.down), out currentRayCastHit);
+                    if ((currentRayCastHit.point - preHit.point).magnitude > draggingThreshold)
+                    {
+                        bendValue += Time.deltaTime * foldingScalingSpeed;
+                        Physics.Raycast(new Ray(foldingObj.transform.position.ModifyThisVector(0, 1, 0), Vector3.down), out currentRayCastHit);
 
 
-                    outputTex = meshUVPainter.PaintOnUV(currentRayCastHit, preHit, paintColor, paintRadious, 10000000, rectengle);
-                    outputMaterial.SetTexture("MaskInput", outputTex);
+                        outputTex = meshUVPainter.PaintOnUV(currentRayCastHit, preHit, paintColor, paintRadious, 10000000, rectengle);
+                        outputMaterial.SetTexture("MaskInput", outputTex);
 
-                    bendValue = Mathf.Clamp01(bendValue);
-                    //foldinMesh.SetBlendShapeWeight(0, bendValue);
-                    foldingObj.transform.localScale = Vector3.Lerp(
-                            foldingObj.transform.localScale,
-                        new Vector3(1, 1, 1), foldingScalingSpeed * Time.deltaTime);
+                        bendValue = Mathf.Clamp01(bendValue);
+                        //foldinMesh.SetBlendShapeWeight(0, bendValue);
+                        foldingObj.transform.localScale = Vector3.Lerp(
+                                foldingObj.transform.localScale,
+                            new Vector3(1, 1, 1), foldingScalingSpeed * Time.deltaTime);
 
-                    foldingScalingSpeed += Time.deltaTime;
+                        foldingScalingSpeed += Time.deltaTime;
 
-                    preHit = currentRayCastHit;
+                        preHit = currentRayCastHit;
+
+                        Sprite sprite = Sprite.Create(outputTex, new Rect(0f, 0f, outputTex.width, outputTex.width), new Vector3(0.5f, 0.5f), 25f);
+                        spriteRenderer.sprite = sprite;
+                        Destroy(polygonCollider2D);
+                        polygonCollider2D = spriteRenderer.gameObject.AddComponent<PolygonCollider2D>();
+                        
+                    }
                 }
             }
         }
@@ -125,8 +138,17 @@ public class MeshEraser : APBehaviour
     #endregion ALL OVERRIDING FUNCTIONS
     //=================================
     #region ALL SELF DECLEAR FUNCTIONS
-    
-    
+
+    bool DraggingOnPaper(Vector3 position)
+    {
+        position.y = position.z;
+        position.z = -10;
+        inputChecker.transform.localPosition = position;
+        RaycastHit2D hit = Physics2D.Raycast(inputChecker.transform.position, inputChecker.transform.forward, 100, 1 << ConstantManager.SENSOR);
+
+        return hit.collider != null;
+    }
+
     #endregion ALL SELF DECLEAR FUNCTIONS
 
 }
